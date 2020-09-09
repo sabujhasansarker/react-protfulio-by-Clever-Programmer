f0e0b878da174c34fdd8f36cf7755011c0d28628
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-09-09 16:20:09 +0600

    Updates

---
+++ precache-manifest.c30f92215a28c4ca666358f70eefb5fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50e67b0de245db800cb52434f3a2a62",
+    "url": "/react-protfulio-by-Clever-Programmer/index.html"
+  },
+  {
+    "revision": "5a42397cc2f7298cffe0",
+    "url": "/react-protfulio-by-Clever-Programmer/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/react-protfulio-by-Clever-Programmer/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-protfulio-by-Clever-Programmer/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a42397cc2f7298cffe0",
+    "url": "/react-protfulio-by-Clever-Programmer/static/js/main.bd2a3079.chunk.js"
+  },
+  {
+    "revision": "0439e82161cb70e37c88",
+    "url": "/react-protfulio-by-Clever-Programmer/static/js/runtime-main.8cc30815.js"
+  }
+]);
